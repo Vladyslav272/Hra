708a88e8ef11c7a4403c33ba6a9c5373007978b1
Vladyslav Voloshchuk <voloshchuk@spstrutnov.cz>
2021-05-16 20:59:13 +0200

    menu v2

--- src/hra.py
+++ src/hra.py
@@ -1,32 +1,88 @@
 import pygame
 import sys
-import pygame_menu
 
 clock = pygame.time.Clock()
+from pygame.locals import *
 pygame.init()
+pygame.display.set_caption('Hra')
 
-sirka_okna = 1200
-vyska_okna = 800
+ 
+font = pygame.font.SysFont(None, 120)
+ 
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+zelena = (0, 112, 0)
+
+golem = pygame.image.load("Golem.png")
+pozadi = pygame.image.load("Cartoon_Forest_BG_01.png")
+pozadi_hra = pygame.image.load("Cartoon_Forest_BG_04.png")
+
+sirka_okna = 1920
+vyska_okna = 1080
+sirka_tlacitka = 300
+vyska_tlacitka = 100
+
+pozice_x = (sirka_okna - sirka_tlacitka) /2
+pozice_y = 200
+pozice_y2 = pozice_y + 200
+
+
 okno = pygame.display.set_mode((sirka_okna, vyska_okna))
-pygame.display.set_caption(("hra"))
 
-def start_the_game():
-    while start_the_game:
+click = False
+ 
+def main_menu():
+    while True:
         
-        #uzivatelske parametry
-        sirka_okna = 1200
-        vyska_okna = 800
-        FPS = 60
-        sirka_ctverce = 20
-        vyska_ctverce = 20
-        pozice_ctverce_x = 800
-        pozice_ctverce_y = 500
-        rychlost = 8
-        cerna = (0, 0, 0)
-        bila = (255, 255, 255)
         
-        okno = pygame.display.set_mode((sirka_okna, vyska_okna))
-
+        okno.blit(pozadi, (0, 0))
+        
+        mx, my = pygame.mouse.get_pos()
+ 
+        button_1 = pygame.Rect(pozice_x, pozice_y, sirka_tlacitka, vyska_tlacitka)
+        button_2 = pygame.Rect(pozice_x, pozice_y2, sirka_tlacitka, vyska_tlacitka)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                vypnout()
+        pygame.draw.rect(okno, (zelena), button_1)
+        pygame.draw.rect(okno, (zelena), button_2)
+        
+        draw_text("Play", font, (255, 255, 255), okno, (pozice_x + 55), (pozice_y + 10))
+        draw_text("Exit", font, (255, 255, 255), okno, (pozice_x + 55), (pozice_y2 + 10))
+        
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        pygame.display.update()
+        clock.tick(60)
+ 
+def game():
+    running = True
+    while running:
+        pozice_golema_x = 50
+        pozice_golema_y = 900
+        sirka_golema = 200
+        vyska_golema = 133
+        rychlost = 10
+        
+        okno.blit(pozadi_hra,(0, 0))
+        
         stisknuto = pygame.key.get_pressed()
         udalosti = pygame.event.get()
         
@@ -35,40 +91,40 @@ def start_the_game():
                 pygame.quit()
                 sys.exit()
         if stisknuto[pygame.K_ESCAPE]:
-            pygame.quit()
-            sys.exit()
+            running = False
+        
         
         #ovladani
         if stisknuto[pygame.K_RIGHT]:
-            pozice_ctverce_x += rychlost
+            pozice_golema_x += rychlost
         if stisknuto[pygame.K_LEFT]:
-            pozice_ctverce_x -= rychlost
+            pozice_golema_x -= rychlost
         if stisknuto[pygame.K_UP]:
-            pozice_ctverce_y -= rychlost
+            pozice_golema_y -= rychlost
         if stisknuto[pygame.K_DOWN]:
-            pozice_ctverce_y += rychlost
-        
+            pozice_golema_y += rychlost
+            
         #omezeni pohybu
-        if pozice_ctverce_x < 0:
-            pozice_ctverce_x = 0
-        if pozice_ctverce_x + sirka_ctverce > sirka_okna:
-            pozice_ctverce_x = sirka_okna - sirka_ctverce
-        if pozice_ctverce_y < 0:
-            pozice_ctverce_y = 0
-        if pozice_ctverce_y + vyska_ctverce > vyska_okna:
-            pozice_ctverce_y = vyska_okna - vyska_ctverce
-                
-        okno.fill(cerna)
-    
-        #objekty
-        ctverec = pygame.draw.rect(okno, bila, ((pozice_ctverce_x, pozice_ctverce_y), (sirka_ctverce, vyska_ctverce)))
+        if pozice_golema_x < 0:
+            pozice_golema_x = 0
+        elif pozice_golema_x + sirka_golema > sirka_okna:
+            pozice_golema_x = sirka_okna - sirka_golema
+        if pozice_golema_y < 0:
+            pozice_golema_y = 0
+        elif pozice_golema_y + vyska_golema > vyska_okna:
+            pozice_golema_y = vyska_okna - vyska_golema
         
-        clock.tick(FPS)
+        okno.blit(pozadi_hra,(0, 0))
+        okno.blit(golem, (pozice_golema_x, pozice_golema_y))
+        
+        
         pygame.display.update()
-    pass    
-menu = pygame_menu.Menu("Welcome", 300, 400, theme=pygame_menu.themes.THEME_BLUE)
-
-menu.add.button('Play', start_the_game)
-menu.add.button('Quit', pygame_menu.events.EXIT)
-
-menu.mainloop(okno)
+        clock.tick(60)
+ 
+def vypnout():
+    running = True
+    while running:
+        pygame.quit()
+        sys.exit()
+ 
+main_menu()
